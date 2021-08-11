38646dd7db4960f72639a997b360a3fc00837d54
shinyay <shinya.com@gmail.com>
2021-08-11 21:58:21 +0900

    Add logger to extend Any as property

--- src/main/kotlin/com/google/shinyay/codelab/TutorialApplication.kt
+++ src/main/kotlin/com/google/shinyay/codelab/TutorialApplication.kt
@@ -1,5 +1,7 @@
 package com.google.shinyay.codelab
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -9,3 +11,6 @@ class TutorialApplication
 fun main(args: Array<String>) {
 	runApplication<TutorialApplication>(*args)
 }
+
+val Any.logger: Logger
+	get() = LoggerFactory.getLogger(this::class.java)
