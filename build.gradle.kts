bf8774cabcddaf97136b65e850a4d9cc2d98415b
shinyay <shinya.com@gmail.com>
2021-08-15 06:39:37 +0900

    Add Spring Data JPA dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.5.21"
 	kotlin("plugin.spring") version "1.5.21"
+	kotlin("plugin.jpa") version "1.5.21"
 }
 
 group = "com.google.shinyay.codelab"
@@ -21,6 +22,7 @@ extra["testcontainersVersion"] = "1.15.3"
 
 dependencies {
 	implementation("com.google.cloud:spring-cloud-gcp-starter")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("mysql:mysql-connector-java")
