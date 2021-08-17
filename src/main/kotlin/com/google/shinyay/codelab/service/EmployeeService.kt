0fcdaf2365c78d0e96bf460ad1280cc2bb07970f
shinyay <shinya.com@gmail.com>
2021-08-17 19:54:08 +0900

    Add find all function

--- src/main/kotlin/com/google/shinyay/codelab/service/EmployeeService.kt
+++ src/main/kotlin/com/google/shinyay/codelab/service/EmployeeService.kt
@@ -1,8 +1,13 @@
 package com.google.shinyay.codelab.service
 
+import com.google.shinyay.codelab.entity.Employee
 import com.google.shinyay.codelab.repository.EmployeeRepository
 import org.springframework.stereotype.Service
 
 @Service
 class EmployeeService(val repository: EmployeeRepository) {
+
+    fun findAllEmployee(): MutableList<Employee> {
+        return repository.findAll()
+    }
 }
