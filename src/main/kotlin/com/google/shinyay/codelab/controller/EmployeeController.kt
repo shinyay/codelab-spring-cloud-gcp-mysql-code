0a5d7d907a9b8bb6a8ec59dba33545d7b0bab178
shinyay <shinya.com@gmail.com>
2021-08-18 11:53:00 +0900

    Add find all employees api

--- src/main/kotlin/com/google/shinyay/codelab/controller/EmployeeController.kt
+++ src/main/kotlin/com/google/shinyay/codelab/controller/EmployeeController.kt
@@ -1,6 +1,9 @@
 package com.google.shinyay.codelab.controller
 
+import com.google.shinyay.codelab.entity.Employee
 import com.google.shinyay.codelab.service.EmployeeService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,5 +11,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1")
 class EmployeeController(val service: EmployeeService) {
+
+    @GetMapping("/employees")
+    fun getAllEmployees(): ResponseEntity<MutableList<Employee>> {
+        return ResponseEntity.status(HttpStatus.OK).body(service.findAllEmployee())
+    }
 
 }
