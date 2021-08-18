61b88db852ce294ccc349755c203645feba9bfb6
shinyay <shinya.com@gmail.com>
2021-08-18 14:12:15 +0900

    Add save employee api

--- src/main/kotlin/com/google/shinyay/codelab/controller/EmployeeController.kt
+++ src/main/kotlin/com/google/shinyay/codelab/controller/EmployeeController.kt
@@ -4,9 +4,7 @@ import com.google.shinyay.codelab.entity.Employee
 import com.google.shinyay.codelab.service.EmployeeService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1")
@@ -15,6 +13,11 @@ class EmployeeController(val service: EmployeeService) {
     @GetMapping("/employees")
     fun getAllEmployees(): ResponseEntity<MutableList<Employee>> {
         return ResponseEntity.status(HttpStatus.OK).body(service.findAllEmployee())
+    }
+
+    @PostMapping("/employees")
+    fun postEmployee(@RequestBody employee: Employee): ResponseEntity<Employee> {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.registerEmployee(employee))
     }
 
 }
