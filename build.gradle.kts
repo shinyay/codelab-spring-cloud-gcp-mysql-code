ccde52f57d852df8469e1e6ecf435f441f77057f
shinyay <shinya.com@gmail.com>
2021-08-18 11:44:53 +0900

    Add Spring Web Dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ extra["testcontainersVersion"] = "1.15.3"
 
 dependencies {
 	implementation("com.google.cloud:spring-cloud-gcp-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
