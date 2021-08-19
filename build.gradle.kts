fc8221b6a9899b499785f4545e985da427760d56
shinyay <shinya.com@gmail.com>
2021-08-19 21:48:06 +0900

    Modify dependency for Spring Cloud GCP for MySQL

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,8 @@ extra["springCloudVersion"] = "2020.0.3"
 extra["testcontainersVersion"] = "1.15.3"
 
 dependencies {
-	implementation("com.google.cloud:spring-cloud-gcp-starter")
+//	implementation("com.google.cloud:spring-cloud-gcp-starter")
+	implementation("com.google.cloud:spring-cloud-gcp-starter-sql-mysql")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
