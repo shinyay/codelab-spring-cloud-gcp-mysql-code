ac01904f3141728778a32c77c5e91a79d396f01f
shinyay <shinya.com@gmail.com>
2021-08-19 09:53:31 +0900

    Modify id column name with Column annotation

--- src/main/kotlin/com/google/shinyay/codelab/entity/Employee.kt
+++ src/main/kotlin/com/google/shinyay/codelab/entity/Employee.kt
@@ -7,7 +7,8 @@ import javax.persistence.*
 data class Employee(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long,
+    val employee_id: Long,
+    @Column(name = "employee_name", nullable = false, length = 64)
     val name: String,
     val role: String,
     val department_id: Long
