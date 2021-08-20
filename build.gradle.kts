f76016ffaa18c3860421cf5d4b980545d362d7b8
shinyay <shinya.com@gmail.com>
2021-08-20 17:34:36 +0900

    Delete MySQL driver dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	runtimeOnly("mysql:mysql-connector-java")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
